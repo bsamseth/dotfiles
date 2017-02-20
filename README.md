3fec6eb388e956a05b343528ad550d883e35cfdc
Bendik Samseth <b.samseth@gmail.com>
2017-02-20 23:24:54 +0100

    Add install script to handle some deps.
    Also set up to try and install a bunch of stuff that
    I have on my main machine now. List should perhaps be filtered,
    and updated once in a while.

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This repository contains a collection of configuration files.
 The aim of this repo is to make swapping computers as easy
 as possible, keeping all settings from one system to another.
+
+
+## Install
+
+Certain dotfiles depend on some things to be installed at seperate places.
+The [install](install) script aims to handle all the necessary things.
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+./bootstrap.sh  # Move all dotfiles into $HOME.
+
+BREWED="aircrack-ng ant homebrew/science/armadillo aspell homebrew/boneyard/clang-omp cmatrix paulp/extras/coursier cowsay crunch exiftool figlet fortune gcc ghc ghostscript gnupg gnupg2 grep gsl ifstat imagemagick ispell lcov libsass macvim homebrew/science/matplotlib mit-scheme multimarkdown node open-mpi perl pypy3 python python3 qcachegrind rainbarf reattach-to-user-namespace reaver rmtrash ruby scipy stockfish homebrew/science/superlu43 tmux trash tree uncrustify utf8proc valgrind vim watch wget zeromq zsh zsh-completions zsh-syntax-highlighting"
+
+
+if [[ $OSTYPE == darwin* ]]; then
+    # Install brew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    # Install a bunch of things
+    brew install "$BREWED"
+else
+    # Assumed to be on linux. Try to apt install the stuff.
+    sudo apt install "$BREWED"
+fi
+
+
+# Oh my zsh
+# Attempt to set zsh as default shell. If this does not work, see https://www.google.com/search?q=zsh+default+without+chsh
+chsh -s $(which zsh)
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+# Tmux powerline
+git clone https://github.com/bsamseth/tmux-powerline.git
+mv tmux-powerline ~/.tmux-powerline
