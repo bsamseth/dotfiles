fe6249f22dbc2f7e0c4195e1b7f8369d7f07bf1f
Bendik Samseth <b.samseth@gmail.com>
2019-10-09 12:49:57 +0200

    Clean up install script.

--- install.sh
+++ install.sh
@@ -2,34 +2,20 @@
 
 ./bootstrap.sh  # Move all dotfiles into $HOME.
 
-BREWED="aircrack-ng ant homebrew/science/armadillo aspell homebrew/boneyard/clang-omp cmatrix paulp/extras/coursier cowsay crunch exiftool figlet fortune gcc ghc ghostscript gnupg gnupg2 grep gsl ifstat imagemagick ispell lcov libsass macvim homebrew/science/matplotlib mit-scheme multimarkdown node open-mpi perl pypy3 python python3 qcachegrind rainbarf reattach-to-user-namespace reaver rmtrash ruby scipy stockfish homebrew/science/superlu43 tmux trash tree uncrustify utf8proc valgrind vim watch wget zeromq zsh zsh-completions zsh-syntax-highlighting neovim/neovim/neovim"
-
-
-if [[ $OSTYPE == darwin* ]]; then
-    # Install brew
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    # Install a bunch of things
-    brew install "$BREWED"
-else
-    # Assumed to be on linux. Try to apt install the stuff.
-    sudo apt install "$BREWED"
-fi
-
-
 # Oh my zsh
 # Attempt to set zsh as default shell. If this does not work, see https://www.google.com/search?q=zsh+default+without+chsh
 chsh -s $(which zsh)
 # Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Get the powerlevel10k zsh theme:
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 # zsh syntax highlighting requires this repo to be in the custom folder. This does the trick.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-
 # Tmux powerline
 git clone https://github.com/bsamseth/tmux-powerline.git
 mv tmux-powerline ~/.tmux-powerline
-
 
 # Bash insulter
 sudo wget -O /etc/bash.command-not-found https://raw.githubusercontent.com/hkbakke/bash-insulter/master/src/bash.command-not-found
